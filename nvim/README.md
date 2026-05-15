3a5b8449357e3e533939acefea5b726f2f0b39b3
Ken Dreyer <kdreyer@redhat.com>
2026-05-15 12:04:42 -0400

    add nvim configuration

---
+++ nvim/README.md
@@ -0,0 +1,3 @@
+This is a directory in https://github.com/ktdreyer/dotfiles.
+
+Symlink this directory in your local Git clone to `~/.config/nvim/`.
---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.title = true
+
+-- unclear if this actually works, per https://github.com/equalsraf/neovim-qt/issues/883
+-- au UIAttach * ++once lua require('ginit')
+
+-- Paste from system clipboard in Insert mode using Ctrl-Shift-V
+vim.keymap.set('i', '<C-S-V>', '<C-r><C-o>++', { noremap = true, desc = 'Paste from clipboard' })
+
+-- Optional: Also make it work in Normal mode and Command-line mode
+vim.keymap.set('n', '<C-S-V>', '"+P', { noremap = true, desc = 'Paste from clipboard' })
+vim.keymap.set('c', '<C-S-V>', '<C-r>+', { noremap = true, desc = 'Paste from clipboard' })
