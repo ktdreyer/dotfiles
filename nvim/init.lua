a68ac9d26a4783ee1f6ff0bcb3aaad770d36be87
Ken Dreyer <kdreyer@redhat.com>
2026-06-22 12:06:23 -0400

    nvim: resume cursor position in files

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,15 @@ vim.keymap.set('i', '<C-S-V>', '<C-r><C-o>+', { noremap = true, desc = 'Paste fr
 -- Optional: Also make it work in Normal mode and Command-line mode
 vim.keymap.set('n', '<C-S-V>', '"+P', { noremap = true, desc = 'Paste from clipboard' })
 vim.keymap.set('c', '<C-S-V>', '<C-r>+', { noremap = true, desc = 'Paste from clipboard' })
+
+-- Check where you were last time, ensure that line hasn't been deleted by an external program, and then put your cursor exactly where you left off.
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
